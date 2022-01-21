d9cce7a75cae949c5f27f9f69ee4d44d61fed479
Jomel Edodollon <jomeledodollon@yahoo.ca>
2022-01-21 00:38:00 -0800

    Complete functionality

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,2 +1,21 @@
 const request = require('request');
+const args = process.argv.slice(2);
+const breedSearch = args[0];
+const website = `https://api.thecatapi.com/v1/breeds/search?q=${breedSearch}`;
 
+
+request(website, (error, response, body) => {
+  if (error) {
+    console.error('error:', error);
+    return;
+  }
+
+  const data = JSON.parse(body);
+
+  if (data.length === 0) {
+    console.log(`${breedSearch} breed not found`);
+    return;
+  }
+
+  console.log(data[0].description);
+});
